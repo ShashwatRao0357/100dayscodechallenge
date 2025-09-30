ffa72213807c02bf485e66ee69db10fd74b67793
Shashwatrao03 <pcgaming123tyr@gmail.com>
2025-10-01 00:39:17 +0530

    Modifies 31dayq2

--- 31dayq2.c
+++ 31dayq2.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 
-int main() {
-    int n;
-    scanf("%d", &n); 
+int revarray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+    int temp;
 
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
     }
+    
+    return 1;
+}
 
-    for (int i = 0; i < n/2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[n - 1 - i];
-        arr[n - 1 - i] = temp;
-    }
-
-    for (int i = 0; i < n; i++) {
+int parray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
+    printf("\n");
+    return 0;
+}
+
+int main() {
+    int size1 = 4;
+    int arr1[] = {1, 2, 3, 4};
+
+    printf("Original array: ");
+    parray(arr1, size1);
+
+    revarray(arr1, size1);
+
+    printf("Reversed array: ");
+    parray(arr1, size1);
 
     return 0;
 }
