00351c7f12fd4b80742d65082f0275c9d4ed4412
Shashwatrao03 <pcgaming123tyr@gmail.com>
2025-11-14 16:20:28 +0530

    day 58

---
+++ 58day.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Enter the number of elements:");
+    scanf("%d", &n);
+    int nums[n];
+    printf("Enter the elements(comma seperated):");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    int answer[n];
+    for(i = 0; i < n; i++) {
+        int prod = 1;
+        for(j = 0; j < n; j++) {
+            if(j != i) {
+                prod *= nums[j];
+            }
+        }
+        answer[i] = prod;
+    }
+    printf("[");
+    for(i = 0; i < n; i++) {
+        if(i == n - 1)
+            printf("%d", answer[i]);
+        else
+            printf("%d,", answer[i]);
+    }
+    printf("]\n");
+    return 0;
+}
