93f68113aacf87a8c10225ccca0de72c0edbd2d8
Shashwatrao03 <pcgaming123tyr@gmail.com>
2025-11-15 14:14:24 +0530

    day 82

---
+++ 82day.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+enum TrafficLight {RED, YELLOW, GREEN};
+
+int main() {
+    char input[10];
+    enum TrafficLight light;
+
+    printf("Enter traffic light color (RED, YELLOW, GREEN): ");
+    scanf("%s", input);
+
+    if(strcmp(input, "RED") == 0) {
+        light = RED;
+    } else if(strcmp(input, "YELLOW") == 0) {
+        light = YELLOW;
+    } else if(strcmp(input, "GREEN") == 0) {
+        light = GREEN;
+    } else {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    switch(light) {
+        case RED:
+            printf("Stop\n");
+            break;
+        case YELLOW:
+            printf("Wait\n");
+            break;
+        case GREEN:
+            printf("Go\n");
+            break;
+    }
+    return 0;
+}
